087cfdb5969a1ebfc9afc6d382b663411c022942
Casey M <caseym@gmail.com>
2022-08-15 08:59:40 -0500

    handle invalid openalex id and no doi

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from collections import OrderedDict
 import os
 
 from elasticsearch_dsl import Search, connections
-from flask import Flask
+from flask import Flask, abort
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
 
 
 # app setup
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config ['JSON_SORT_KEYS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JSON_SORT_KEYS"] = False
 db = SQLAlchemy(app)
-connections.create_connection(hosts=[os.environ.get('ES_URL_PROD')], timeout=30)
+connections.create_connection(hosts=[os.environ.get("ES_URL_PROD")], timeout=30)
 
 
 # models
@@ -53,14 +53,34 @@ class MessageSchema(Schema):
 
 
 # views
-@app.route('/works/<openalex_id>/ngrams')
+@app.route("/works/<openalex_id>/ngrams")
 def ngrams_view(openalex_id):
     result = OrderedDict()
     doi = openalex_id_to_doi(openalex_id)
-    short_doi = doi.replace("https://doi.org/", "")
-    ngrams = Ngram.query.filter_by(doi=short_doi).first()
-    result["meta"] = {"count": len(ngrams.json_ngrams), "doi": doi, "openalex_id": f"https://openalex.org/{openalex_id}"}
-    result["ngrams"] = ngrams.json_ngrams
+    if doi:
+        short_doi = doi.replace("https://doi.org/", "")
+        ngrams = Ngram.query.filter_by(doi=short_doi).first()
+        if ngrams:
+            result["meta"] = {
+                "count": len(ngrams.json_ngrams),
+                "doi": doi,
+                "openalex_id": f"https://openalex.org/{openalex_id}",
+            }
+            result["ngrams"] = ngrams.json_ngrams
+        else:
+            result["meta"] = {
+                "count": 0,
+                "doi": doi,
+                "openalex_id": f"https://openalex.org/{openalex_id}",
+            }
+            result["ngrams"] = []
+    else:
+        result["meta"] = {
+            "count": 0,
+            "doi": None,
+            "openalex_id": f"https://openalex.org/{openalex_id}",
+        }
+        result["ngrams"] = []
     message_schema = MessageSchema()
     return message_schema.dump(result)
 
@@ -73,10 +93,13 @@ def openalex_id_to_doi(openalex_id):
     s = s.source(["id", "doi"])
     s = s.filter("term", id=openalex_id)
     response = s.execute()
+    if s.count() == 0:
+        # openalex id is not valid
+        abort(404)
     for r in response:
         doi = r.doi
         return doi
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
